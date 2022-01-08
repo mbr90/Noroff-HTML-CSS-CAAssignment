bb1061a98c740b82027de32198fb20fb60b0950c
Mbr90 <49838649+Mbr90@users.noreply.github.com>
2022-01-08 23:53:55 +0100

    Fixed Log In

--- script/logIn.js
+++ script/logIn.js
@@ -1,37 +1,18 @@
 const form = document.querySelector("#contactForm");
 const success = document.querySelector(".formSuccess");
-const name = document.querySelector("#name");
-const nameError = document.querySelector("#name-error");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#email-error");
-const address = document.querySelector("#address");
-const addressError = document.querySelector("#address-error");
 const password = document.querySelector("#password");
 const passwordError = document.querySelector("#password-error");
-const loading = document.querySelector(".loadingForm");
-const loadingMessage = document.querySelector(".loadingMessage");
 
 function formInputValidation(event) {
   event.preventDefault();
-
-  if (lenControll(name.value, 0) === true) {
-    nameError.style.display = "none";
-  } else {
-    nameError.style.display = "block";
-  }
 
   if (emailValidation(email.value) === true) {
     emailError.style.display = "none";
   } else {
     emailError.style.display = "block";
   }
-
-  if (lenControll(address.value, 10) === true) {
-    addressError.style.display = "none";
-  } else {
-    addressError.style.display = "block";
-  }
-
   if (lenControll(password.value, 8) === true) {
     passwordError.style.display = "none";
   } else {
@@ -40,16 +21,11 @@ function formInputValidation(event) {
 
   if (
     lenControll(password.value, 8) === true &&
-    lenControll(address.value, 10) === true &&
-    emailValidation(email.value) === true &&
-    lenControll(name.value, 0) === true
+    emailValidation(email.value) === true
   ) {
-    success.style.display = "block";
-    loading.style.display = "block";
-    loadingMessage.style.display = "block";
     window.setTimeout(function () {
       window.location.href = "browseFilms.html";
-    }, 2000);
+    }, 200);
   } else {
     success.style.display = "none";
   }
