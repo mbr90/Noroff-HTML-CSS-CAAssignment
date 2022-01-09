4391ced38c5c54625c40a6105072534fbc89e8f8
Mbr90 <49838649+Mbr90@users.noreply.github.com>
2022-01-09 14:14:53 +0100

    Changes

--- script/purchaseCart.js
+++ script/purchaseCart.js
@@ -5,7 +5,7 @@ const url =
   "https://api.themoviedb.org/3/movie/" +
   id +
   "?language=en-US&api_key=c7a9a6147cc43aac6069597872e5590b&=634649";
-const queryResult = document.querySelector(".queryResult");
+const queryResult = document.querySelector(".purchase");
 const pageTitle = document.querySelector("title");
 
 async function apiCall() {
@@ -16,18 +16,16 @@ async function apiCall() {
 
     queryResult.innerHTML = "";
     pageTitle.innerHTML = "";
-    pageTitle.innerHTML += `${movie.title}`;
-    queryResult.innerHTML += `<div class="resultDet">
-        <h2>${movie.original_title}</h2>
-        <div class = "filmPoster"><img src="https://image.tmdb.org/t/p/w185/${movie.poster_path}" alt="${movie.original_title}" /></div>
-        <p><b>Score:</b> ${movie.vote_average}</p>
-        <p class="filmSum"><b>Overview:</b>${movie.overview}</p>
+    pageTitle.innerHTML += `Purchase and Download: ${movie.title}`;
+    queryResult.innerHTML += `<div class="purchase">
+    <div class = "filmPoster"><img src="https://image.tmdb.org/t/p/w45/${movie.poster_path}" alt="${movie.original_title}" /></div><h2>${movie.original_title}</h2> <button class="removeButton">Remove</button>
       </div>`;
   } catch (error) {
     console.log(error);
-
     queryResult.innerHTML = `<div class="errMsg">An error occured trying to get results from the API</div>`;
   }
 }
 
 apiCall();
+
+console.log(queryResult);
